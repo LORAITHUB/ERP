41d5fc739a39709bd0a6de88ae471adaef0c64a1
naveen-chintapalli <chintapalli@lorainnovations.com>
2025-09-25 16:03:26 +0530

    update

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+psycopg2://postgres:Siva%40265@localhost/postgres")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+psycopg2://postgres:root@localhost/hospitalitydb")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
